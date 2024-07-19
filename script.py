fe2a4d67ec38ce9a4ebd1ddbebceb0033135660f
Chelz1990 <96008723+Chelz1990@users.noreply.github.com>
2024-07-19 08:34:21 -0400

    small changes

--- script.py
+++ script.py
@@ -11,7 +11,7 @@ movies = []
 
 # script to get user input
 addmovie = input("type 1 to add a show/movie to the list, 2 to select a random move, or 3 to quit: ")
-while addmovie == '1' or addmovie == '3' or addmovie == '2':
+while addmovie == '1' or addmovie == '3' or addmovie == '2' :
     if addmovie == '1':
         movies.append(input("Okay! Please enter the title of the show/movie: "))
     elif addmovie == '3':
@@ -20,10 +20,12 @@ while addmovie == '1' or addmovie == '3' or addmovie == '2':
     elif addmovie == '2':
         break
     addmovie = input("type 1 to add a show/movie to the list, 2 to select a random move, or 3 to quit: ")
-
-
+        
+               
 # selection of random choice after user input
 random_movie = random.choice(movies)
 print (random_movie)
 
 #selection of random choice without user input
+
+
